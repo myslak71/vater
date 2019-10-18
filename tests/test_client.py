5dc36c54f3ece169943c5eb9b3d965a7eecb41cc
myslak71 <myslak@protonmail.com>
2019-10-18 19:35:58 +0200

    Add test cases

--- tests/test_client.py
+++ tests/test_client.py
@@ -106,7 +106,7 @@ class TestSubjectSearch:
 
     @responses.activate
     def test_search_nip(self, client):
-        """Test that proper nip and request identifier are returned for valid nip."""
+        """Test proper subject and request identifier are returned for valid nip."""
         self.set_up()
         responses.add(
             responses.GET,
@@ -128,7 +128,7 @@ class TestSubjectSearch:
 
     @responses.activate
     def test_search_nips(self, client):
-        """Test that proper nips and request identifier are returned for valid nips."""
+        """Test proper subjects and request identifier are returned for valid nips."""
         self.set_up()
         responses.add(
             responses.GET,
@@ -143,18 +143,18 @@ class TestSubjectSearch:
             content_type="application/json",
         )
 
-        assert client.search_nips("6969696969", datetime.date(2001, 1, 1)) == (
+        assert client.search_nips(["6969696969"], datetime.date(2001, 1, 1)) == (
             [self.example_subject],
             "aa111-aa111aaa",
         )
 
     @responses.activate
     def test_search_regon(self, client):
-        """Test that proper regon and request identifier are returned for valid regon."""
+        """Test proper subject and request identifier are returned for valid regon."""
         self.set_up()
         responses.add(
             responses.GET,
-            "https://test-api.no/api/search/regon/6969696969?date=2001-01-01",
+            "https://test-api.no/api/search/regon/696969696?date=2001-01-01",
             status=200,
             json={
                 "result": {
@@ -165,7 +165,111 @@ class TestSubjectSearch:
             content_type="application/json",
         )
 
-        assert client.search_nip("6969696969", datetime.date(2001, 1, 1)) == (
+        assert client.search_regon("696969696", datetime.date(2001, 1, 1)) == (
             self.example_subject,
             "aa111-aa111aaa",
         )
+
+    @responses.activate
+    def test_search_regons(self, client):
+        """Test proper subjects and request identifier are returned for valid regons."""
+        self.set_up()
+        responses.add(
+            responses.GET,
+            "https://test-api.no/api/search/regons/696969696?date=2001-01-01",
+            status=200,
+            json={
+                "result": {
+                    "subjects": [self.example_subject_dict],
+                    "requestId": "aa111-aa111aaa",
+                }
+            },
+            content_type="application/json",
+        )
+
+        assert client.search_regons(["696969696"], datetime.date(2001, 1, 1)) == (
+            [self.example_subject],
+            "aa111-aa111aaa",
+        )
+
+    @responses.activate
+    def test_search_account(self, client):
+        """Test proper subject and request identifier are returned for valid account."""
+        self.set_up()
+        responses.add(
+            responses.GET,
+            f"https://test-api.no/api/search/bank-account/{13 * '69'}?date=2001-01-01",
+            status=200,
+            json={
+                "result": {
+                    "subjects": [self.example_subject_dict],
+                    "requestId": "aa111-aa111aaa",
+                }
+            },
+            content_type="application/json",
+        )
+
+        assert client.search_account(f"{13 * '69'}", datetime.date(2001, 1, 1)) == (
+            [self.example_subject],
+            "aa111-aa111aaa",
+        )
+
+    @responses.activate
+    def test_search_accounts(self, client):
+        """Test proper subject and request identifier are returned for valid accounts."""
+        self.set_up()
+        responses.add(
+            responses.GET,
+            f"https://test-api.no/api/search/bank-accounts/{13 * '69'}?date=2001-01-01",
+            status=200,
+            json={
+                "result": {
+                    "subjects": [self.example_subject_dict],
+                    "requestId": "aa111-aa111aaa",
+                }
+            },
+            content_type="application/json",
+        )
+
+        assert client.search_accounts([f"{13 * '69'}"], datetime.date(2001, 1, 1)) == (
+            [self.example_subject],
+            "aa111-aa111aaa",
+        )
+
+    @responses.activate
+    def test_check_nip(self, client):
+        """Test proper tuple is returned for valid nip and account."""
+        self.set_up()
+        responses.add(
+            responses.GET,
+            (
+                "https://test-api.no/api/check/nip/6969696969/bank-account/"
+                f"{13 * '69'}?date=2001-01-01"
+            ),
+            status=200,
+            json={"result": {"accountAssigned": "TAK", "requestId": "aa111-aa111aaa"}},
+            content_type="application/json",
+        )
+
+        assert client.check_nip(
+            "6969696969", datetime.date(2001, 1, 1), f"{13 * '69'}"
+        ) == (True, "aa111-aa111aaa")
+
+    @responses.activate
+    def test_check_regon(self, client):
+        """Test proper tuple is returned for valid regon and account."""
+        self.set_up()
+        responses.add(
+            responses.GET,
+            (
+                "https://test-api.no/api/check/regon/696969696/bank-account/"
+                f"{13 * '69'}?date=2001-01-01"
+            ),
+            status=200,
+            json={"result": {"accountAssigned": "TAK", "requestId": "aa111-aa111aaa"}},
+            content_type="application/json",
+        )
+
+        assert client.check_regon(
+            "696969696", datetime.date(2001, 1, 1), f"{13 * '69'}"
+        ) == (True, "aa111-aa111aaa")
