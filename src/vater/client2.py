605a00362f08ff9de3107988f2d700a0dcee7a50
myslak71 <43068450+myslak71@users.noreply.github.com>
2019-10-25 14:55:00 +0200

    Refactor

--- src/vater/client2.py
+++ src/vater/client2.py
@@ -19,7 +19,7 @@ class RequestType(Enum):
 
 
 def handle_response(
-    response, request_type: RequestType, many: bool = False
+        response, request_type: RequestType, many: bool = False
 ) -> Union[Tuple[bool, str], Tuple[Union[Subject, List[Subject]], str]]:
     """Handle API response."""
     if response.status_code == 400:
@@ -39,7 +39,7 @@ def handle_response(
 
 
 def api_request(
-    url_pattern: str, request_type: RequestType, many: bool = False
+        url_pattern: str, request_type: RequestType, many: bool = False
 ) -> Callable:
     """Decorate for api requests."""
 
@@ -48,7 +48,7 @@ def api_request(
 
         @functools.wraps(func)
         def inner_wrapper(
-            self, parameter: str, date: datetime.date, account: str = None
+                self, parameter: str, date: datetime.date, account: str = None
         ) -> Union[Tuple[bool, str], Tuple[Union[Subject, List[Subject]], str]]:
             """Fetch subject/subjects and request identifier from API."""
             url = prepare_url(account, date, parameter)
@@ -62,7 +62,7 @@ def api_request(
             return handle_response(response, request_type, many)
 
         def prepare_url(
-            account: Optional[str], date: datetime.date, parameter: str
+                account: Optional[str], date: datetime.date, parameter: str
         ) -> str:
             """Evaluate path parameters."""
             nonlocal url_pattern
@@ -86,12 +86,93 @@ def api_request(
     return outer_wrapper
 
 
+def handle_response2(response, handler_class):
+    """TODO"""
+    if response.status_code == 400:
+        raise InvalidRequestData(ERROR_CODE_MAPPING[response.json()["code"]])
+    elif response.status_code != 200:
+        raise UnknownExternalApiError(response.status_code, response.text)
+
+
+def api_request2(
+        url_pattern: str, handler_class
+) -> Callable:
+    """Decorate for api requests."""
+
+    def outer_wrapper(func: Callable) -> Callable:
+        """Allow passing arguments."""
+
+        handler = handler_class(url=url_pattern)
+
+        @functools.wraps(func)
+        def inner_wrapper(
+                self, parameter: str, date: datetime.date, account: str = None
+        ) -> Union[Tuple[bool, str], Tuple[Union[Subject, List[Subject]], str]]:
+            """Fetch subject/subjects and request identifier from API."""
+
+            
+            url = handler.prepare_url(account, date, parameter)
+
+            response = requests.get(
+                f"{self.base_url}{url}",
+                # without User-Agent header production API returns 403
+                headers={"User-Agent": ""},
+            )
+
+            return handle_response2(response, handler_class)
+
+        def prepare_url(
+                account: Optional[str], date: datetime.date, parameter: str
+        ) -> str:
+            """Evaluate path parameters."""
+            nonlocal url_pattern
+
+            if request_type == RequestType.CHECK:
+                url_pattern = url_pattern.replace(
+                    f"{{{inspect.getfullargspec(func).args[3]}}}",
+                    account,  # type: ignore
+                )
+            else:
+                if many and not isinstance(parameter, str):
+                    parameter = ",".join(parameter)
+
+            url_pattern = url_pattern.replace(
+                f"{{{inspect.getfullargspec(func).args[1]}}}", parameter
+            )
+            return url_pattern.replace("{date}", str(date))
+
+        return inner_wrapper
+
+    return outer_wrapper
+
+
+class CheckRequest:
+    """TODO"""
+
+    def __init__(self, url_pattern):
+        """TODO"""
+        self.url_pattern = url_pattern
+
+    def prepare_url(self, account, date, parameter):
+        """TODO"""
+
+    def validate(self):
+        """TODO"""
+
+    def send_request(self):
+        """TODO"""
+
+
 class Client:
     """Vat register client class."""
 
     def __init__(self, base_url: str) -> None:
         """Set API url."""
         self.base_url = base_url
+
+    @api_request("/api/search/nip/{nip}?date={date}", CheckRequest)
+    def search_nip2(self, nip: str, date: datetime.date) -> Tuple[Subject, str]:
+        """Get detailed vat payer information for given nip."""
 
     @api_request("/api/search/nip/{nip}?date={date}", request_type=RequestType.SEARCH)
     def search_nip(self, nip: str, date: datetime.date) -> Tuple[Subject, str]:
@@ -103,7 +184,7 @@ class Client:
         many=True,
     )
     def search_nips(
-        self, nips: Iterable[str], date: datetime.date
+            self, nips: Iterable[str], date: datetime.date
     ) -> Tuple[List[Subject], str]:
         """Get a list of detailed vat payers information."""
 
@@ -119,7 +200,7 @@ class Client:
         many=True,
     )
     def search_regons(
-        self, regons: Iterable[str], date: datetime.date
+            self, regons: Iterable[str], date: datetime.date
     ) -> Tuple[List[Subject], str]:
         """Get a list of detailed vat payers information."""
 
@@ -137,7 +218,7 @@ class Client:
         many=True,
     )
     def search_accounts(
-        self, accounts: Iterable[str], date: datetime.date
+            self, accounts: Iterable[str], date: datetime.date
     ) -> Tuple[List[Subject], str]:
         """Get a list of detailed vat payers information."""
 
@@ -146,7 +227,7 @@ class Client:
         request_type=RequestType.CHECK,
     )
     def check_nip(
-        self, nip: str, date: datetime.date, account: str
+            self, nip: str, date: datetime.date, account: str
     ) -> Tuple[bool, str]:
         """Check if given account is assigned to subject with given nip."""
 
@@ -155,6 +236,27 @@ class Client:
         request_type=RequestType.CHECK,
     )
     def check_regon(
-        self, regon: str, date: datetime.date, account: str
+            self, regon: str, date: datetime.date, account: str
     ) -> Tuple[bool, str]:
         """Check if given account is assigned to subject with given regon."""
+
+
+client = Client(base_url="https://wl-api.mf.gov.pl")
+
+subject = client.search_nip("9542682325", "2019-09-30")
+print(subject)
+subjects = client.search_nips(["9542682325"], "2019-09-30")
+print(subjects)
+subject = client.search_regon("241234369", "2019-09-30")
+print(subject)
+subjects = client.search_regons(["241234369"], "2019-09-30")
+print(subjects)
+subject = client.search_account("23249000050000460042096848", "2019-09-30")
+print(subject)
+subject = client.search_accounts(["23249000050000460042096848"], "2019-09-30")
+print(subject)
+
+res = client.check_nip("9542682325", "2019-09-30", "23249000050000460042096848")
+print(res)
+res = client.check_regon("241234369", "2019-09-30", "23249000050000460042096848")
+print(res)
