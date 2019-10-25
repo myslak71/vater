6d1d7e812399413143555d79d161eee9a0646fec
myslak71 <myslak@protonmail.com>
2019-10-26 00:32:51 +0200

    Refactor

---
+++ src/vater/api_request.py
@@ -0,0 +1,37 @@
+"""API request decorator module."""
+import functools
+import inspect
+from typing import Type, Callable, Union, Tuple, List
+
+from vater.models import Subject
+from vater.request_types import RequestType
+
+
+def api_request(
+    url_pattern: str, handler_class: Type[RequestType], **kwargs
+) -> Callable:
+    """Decorate for api requests."""
+    handler = handler_class(url_pattern=url_pattern, **kwargs)
+
+    def decorator_api_request(func: Callable) -> Callable:
+        """Allow passing arguments."""
+
+        @functools.wraps(func)
+        def wrapper_api_request(
+            *args: tuple, **kwargs: dict
+        ) -> Union[Tuple[bool, str], Tuple[Union[Subject, List[Subject]], str]]:
+            """Fetch subject/subjects and request identifier from API."""
+            signature = inspect.signature(func)
+
+            # add not present keywords with their default value
+            for parameter in signature.parameters:
+                if parameter not in kwargs and parameter != "self":
+                    kwargs[parameter] = signature.parameters[parameter].default
+
+            handler.register_args(*args, **kwargs)
+
+            return handler.result()
+
+        return wrapper_api_request
+
+    return decorator_api_request
--- src/vater/client.py
+++ src/vater/client.py
@@ -4,38 +4,9 @@ import functools
 import inspect
 from typing import Callable, Iterable, List, Optional, Tuple, Type, Union
 
+from vater.api_request import api_request
 from vater.models import Subject
 from vater.request_types import CheckRequest, RequestType, SearchRequest
-
-
-def api_request(
-    url_pattern: str, handler_class: Type[RequestType], **kwargs
-) -> Callable:
-    """Decorate for api requests."""
-    handler = handler_class(url_pattern=url_pattern, **kwargs)
-
-    def decorator_api_request(func: Callable) -> Callable:
-        """Allow passing arguments."""
-
-        @functools.wraps(func)
-        def wrapper_api_request(
-            *args: tuple, **kwargs: dict
-        ) -> Union[Tuple[bool, str], Tuple[Union[Subject, List[Subject]], str]]:
-            """Fetch subject/subjects and request identifier from API."""
-            signature = inspect.signature(func)
-
-            # add not present keywords with their default value
-            for parameter in signature.parameters:
-                if parameter not in kwargs and parameter != "self":
-                    kwargs[parameter] = signature.parameters[parameter].default
-
-            handler.register_args(*args, **kwargs)
-
-            return handler.result()
-
-        return wrapper_api_request
-
-    return decorator_api_request
 
 
 class Client:
